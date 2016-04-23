efeb629d7ffa8d7b95340500b923fe585196cd26
nich2000 <nich2000@mail.ru>
2016-04-23 08:58:39 +0300

    fixed installcmake

--- cmd/installcmake.sh
+++ cmd/installcmake.sh
@@ -1,5 +1,8 @@
+sudo apt-get remove --auto-remove cmake
+sudo apt-get purge --auto-remove cmake
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:george-edison55/cmake-3.x
-#sudo apt-get update
+sudo apt-get update
 sudo apt-get install cmake
+sudo apt-get install libpthread-stubs0-dev
 sudo apt-get upgrade
