d1cd1c7f97e7bafafc2656e0ceb7d228db13cccf
nich2000 <nich2000@mail.ru>
2016-04-25 21:05:13 +0300

    Refactor

--- src_c/main.c
+++ src_c/main.c
@@ -41,64 +41,80 @@ int main(int argc, char *argv[])
   char command[256];
   if(argc > 1)
   {
-    // 0     1  2 3     4 5
-    // name -c -p 5700 -h 127.0.0.1
-    if(argc > 3)
-      if(strcmp(argv[2], "-p") == 0)
-        cmd_server_port = atoi(argv[3]);
 
-    if(strcmp(argv[1], "-s") == 0)
+    for(int i = 1; i < argc; i++)
     {
-      log_add(LOG_INFO, "server mode");
-
-      sprintf(command, "server on %d", cmd_server_port);
-      handle_command_str(NULL, command);
-
-      sprintf(command, "webserver on %d", web_server_port);
-      handle_command_str(NULL, command);
-
-      sprintf(command, "wsserver on %d", ws_server_port);
-      handle_command_str(NULL, command);
+      //-d --cmdport   cmd_server_port
+      //-b --webport   web_server_port
+      //-w --wsport    ws_server_port
+      //-h --cmdhost   cmd_server_host
+      //-c --client    client_mode
+      //-t --test      cmp_clients_count
+      //-s --server    server_mode
+      //-e --web       web_server_mode
+      //-o --ws        web_socket_mode
+//    // name -c -p 5700 -h 127.0.0.1
+//    if(argc > 3)
+      //-a --all       all_mode
     }
-    else if(strcmp(argv[1], "-c") == 0)
-    {
-      log_add(LOG_INFO, "client mode");
 
-      if(argc > 5)
-        if(strcmp(argv[4], "-h") == 0)
-          strcpy((char*)cmd_server_host, argv[5]);
+    //      if(strcmp(argv[2], "-p") == 0)
+    //        cmd_server_port = atoi(argv[3]);
 
-      sprintf(command, "client on %d %s 1", cmd_server_port, cmd_server_host);
-      handle_command_str(NULL, command);
-    }
-    else
-    {
+    //    if(strcmp(argv[1], "-s") == 0)
+    //    {
+        //    // 0     1  2 3     4 5
+//      log_add(LOG_INFO, "server mode");
+
+//      sprintf(command, "server on %d", cmd_server_port);
+//      handle_command_str(NULL, command);
+
+//      sprintf(command, "webserver on %d", web_server_port);
+//      handle_command_str(NULL, command);
+
+//      sprintf(command, "wsserver on %d", ws_server_port);
+//      handle_command_str(NULL, command);
+//    }
+//    else if(strcmp(argv[1], "-c") == 0)
+//    {
+//      log_add(LOG_INFO, "client mode");
+
+//      if(argc > 5)
+//        if(strcmp(argv[4], "-h") == 0)
+//          strcpy((char*)cmd_server_host, argv[5]);
+
+//      sprintf(command, "client on %d %s 1", cmd_server_port, cmd_server_host);
+//      handle_command_str(NULL, command);
+//    }
+//    else
+//    {
       make_last_error(ERROR_NONE, errno, "unknown mode");
       goto exit;
-    }
+//    }
   }
   else
   {
     log_add(LOG_INFO, "command mode");
-    while(1)
+  }
+
+  while(1)
+  {
+    fgets(command, sizeof(command), stdin);
+    switch(handle_command_str(NULL, command))
     {
-      fgets(command, sizeof(command), stdin);
-      switch(handle_command_str(NULL, command))
+      case EXEC_NONE:
       {
-        case EXEC_NONE:
-        {
-          make_last_error(ERROR_NONE, errno, "exit by user command");
-          goto exit;
-        }
-        case EXEC_UNKNOWN:
-          log_add_fmt(LOG_CMD, "unknown command: %s",
-                      command);
-          break;
-        case EXEC_DONE:
-          log_add_fmt(LOG_CMD, "done command: %s",
-                      command);
-          break;
+        make_last_error(ERROR_NONE, errno, "exit by user command");
+        goto exit;
       }
+      case EXEC_UNKNOWN:
+        log_add_fmt(LOG_CMD, "unknown command: %s",
+                    command);
+        break;
+      case EXEC_DONE:
+        log_add_fmt(LOG_CMD, "done command: %s",
+                    command);
+        break;
     }
   }
 
