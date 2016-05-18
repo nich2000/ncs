7d17d0365f43ce5320e4b7f9fdf7da7d58ceef5a
nich <nich2000@mail.ru>
2016-05-18 10:38:02 +0300

    Fix some bash scripts

--- cmd/installcmake.sh
+++ cmd/installcmake.sh
@@ -8,21 +8,18 @@ sudo apt-get remove --auto-remove cmake
 sudo apt-get purge --auto-remove cmake
 #------------------------------------------------
 # Install from repository - ubuntu
-# sudo add-apt-repository ppa:george-edison55/cmake-3.x
-# sudo apt-get update
-# sudo apt-get install cmake
-# sudo apt-get upgrade
+sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt-get update
+sudo apt-get install cmake
+sudo apt-get upgrade
 #------------------------------------------------
 # Install from src - debian
-wget https://cmake.org/files/v3.2/cmake-3.2.1-1-src.tar.bz2
-tar xvjf cmake-3.2.1-1-src.tar.bz2
-sh ./cmake-3.2.1-1.sh
+#wget https://cmake.org/files/v3.2/cmake-3.2.1-1-src.tar.bz2
+#tar xvjf cmake-3.2.1-1-src.tar.bz2
+#sh ./cmake-3.2.1-1.sh
 #------------------------------------------------
-#./bootstrap # ?
-#sudo make install
-#------------------------------------------------
-export PATH=/usr/local/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+#export PATH=/usr/local/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 #------------------------------------------------
 cmake --version
 #------------------------------------------------
---
+++ cmd/installneed.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+sudo apt-get install ssh
+sudo apt-get install mc
+sudo apt-get install git
+sudo apt-get install python-dev
